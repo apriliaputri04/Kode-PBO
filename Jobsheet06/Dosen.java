a86cca4fe046eb4fc08ed8a221a35879e97852fa
apriliaputri04 <apriliaputri8057@gmail.com>
2024-10-01 18:34:42 +0700

    J06 P5

--- Jobsheet06/Dosen.java
+++ Jobsheet06/Dosen.java
@@ -4,7 +4,6 @@ public class Dosen extends Pegawai {
     public String nidn;
 
     public Dosen() {
-        System.out.println(gaji);
         System.out.println("Objek dari class Dosen dibuat");
     }
 
